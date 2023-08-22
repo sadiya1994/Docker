44e64b18f908d0892573671507d52ecfe2810c6d
sadiya1994 <118334716+sadiya1994@users.noreply.github.com>
2023-08-22 15:14:16 +0530

    Update server-bucket-log.tf
    did changes in tf state file

--- Terraform/server-bucket-log.tf
+++ Terraform/server-bucket-log.tf
@@ -1,32 +1,17 @@
 provider "aws" {
 
-  region ="us-west-1"
-  access_key="AKIAT7YCB6LKMGNLMOOU"
-  secret_key="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"
+    region ="us-west-1"
+    access_key="AKIAT7YCB6LKMGNLMOOU"
+    secret_key="2jc/STuduma0szh6pozt03x9XqaXYbPaRgF5Pe1N"
 
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-example-bucket-2736273"
-}
+#resource to create bucket
+resource "aws_s3_bucket" "my-califo-14032257" {
+  bucket = "sadiya-0986"
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.example.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = "my-tf-log-bucket-1736735"
-}
-
-resource "aws_s3_bucket_acl" "log_bucket_acl" {
-  bucket = aws_s3_bucket.log_bucket.id
-  acl    = "log-delivery-write"
-}
-
-resource "aws_s3_bucket_logging" "example" {
-  bucket = aws_s3_bucket.example.id
-
-  target_bucket = aws_s3_bucket.log_bucket.id
-  target_prefix = "log/"
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
